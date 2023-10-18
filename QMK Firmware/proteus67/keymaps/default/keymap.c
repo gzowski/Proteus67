2135481889ceff60036f3d52d7eb63dbd08f4fbe
James Gzowski <flexyjerkov@gmail.com>
2023-10-18 11:55:29 +0100

    Wed Oct 18 11:55:29 AM BST 2023

--- QMK Firmware/proteus67/keymaps/default/keymap.c
+++ QMK Firmware/proteus67/keymaps/default/keymap.c
@@ -17,27 +17,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , 			KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC  , KC_ESC ,
 	KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , 			KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC  , KC_ESC ,
         KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , 			KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC  , KC_ESC ,
-        KC_ESC , KC_ESC , KC_LGUI , KC_ALT , KC_SPC , KC_R , _______			KC_COMM , KC_ESC , KC_BSPC , KC_ , KC_ESC , _______ , _______
+        KC_ESC , KC_ESC , KC_LGUI , KC_ALT , KC_SPC ,KC_MUTE , _______			KC_COMM , KC_ESC , KC_BSPC , KC_ , KC_ESC , _______ , _______
 );
     [_LOWER] = LAYOUT_5x14
         KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_ESC ,                  KC_ESC , KC_6   , KC_7   , KC_8   , KC_9   , KC_0    , KC_ESC ,
         KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC ,                  KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC  , KC_ESC ,
         KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC ,                  KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC  , KC_ESC ,
         KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC ,                  KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC  , KC_ESC ,
-        KC_ESC , KC_ESC , KC_LGUI , KC_ALT , KC_SPC , KC_R , _______                    KC_COMM , KC_ESC , KC_BSPC , KC_ , KC_ESC , _______ , _______
+        KC_ESC , KC_ESC , KC_LGUI , KC_ALT , KC_SPC , RGB_MOD , _______                    KC_COMM , KC_ESC , KC_BSPC , KC_ , KC_ESC , _______ , _______
 );
     [_RAISE] = LAYOUT_5x14
         KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_ESC ,                  KC_ESC , KC_6   , KC_7   , KC_8   , KC_9   , KC_0    , KC_ESC ,
         KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC ,                  KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC  , KC_ESC ,
         KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC ,                  KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC  , KC_ESC ,
         KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC ,                  KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC , KC_ESC  , KC_ESC ,
-        KC_ESC , KC_ESC , KC_LGUI , KC_ALT , KC_SPC , KC_R , _______                    KC_COMM , KC_ESC , KC_BSPC , KC_ , KC_ESC , _______ , _______
+        KC_ESC , KC_ESC , KC_LGUI , KC_ALT , KC_SPC , RGB_MOD , _______                    KC_COMM , KC_ESC , KC_BSPC , KC_ , KC_ESC , _______ , _______
 )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_QWERTY] = { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
+    [_QWERTY] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [_LOWER] =  { ENCODER_CCW_CW(RGB_HUD, RGB_HUI),  },
     [_RAISE] =  { ENCODER_CCW_CW(RGB_VAD, RGB_VAI),  }
 };
